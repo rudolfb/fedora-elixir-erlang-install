74eb0e3433732844a664dee4da4213c8e73037b3
Rudolf Bargholz <rudolf@bargholz.ch>
2017-05-17 14:03:20 +0200

    Savepoint

--- uninstall-sublime-text-3.sh
+++ uninstall-sublime-text-3.sh
@@ -1,2 +1,7 @@
 sudo rm /usr/local/bin/subl
 sudo rm -r /usr/local/sublime_text_3
+sudo rm /usr/share/applications/sublime_text.desktop
+
+sudo rm /home/rb/Documents/github/fedora-elixir-erlang-install/sublime_text_3_build_3126_x64.tar.bz2
+sudo rm -r /home/rb/Documents/github/fedora-elixir-erlang-install/sublime_text_3
+
