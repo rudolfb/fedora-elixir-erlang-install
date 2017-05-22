c8156127d288efdbab2cd2285343b205df64f548
Rudolf Bargholz <rudolf@bargholz.ch>
2017-05-22 22:11:50 +0200

    reload shell reload shell to allow direct access to elm, elixir and relang without closing and reopening the shell

--- elixir-erlang.sh
+++ elixir-erlang.sh
@@ -42,6 +42,8 @@ asdf_install "erlang"
 asdf_install "elixir"
 asdf_install "elm"
 
+source ~/.bash_profile
+
 # git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
 # echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 # echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
